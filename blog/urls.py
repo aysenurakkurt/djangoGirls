37cbd4e4449850765a1f6bfc065de1e7bb78cf17
aysenurakkurt <akkurt.aysenur1@gmail.com>
2017-10-14 17:48:48 +0300

    Fixed css file

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from mysite import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
